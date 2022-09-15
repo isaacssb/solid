4f58bdb98e030360170e99e7bdaa5b8c69ca63ad
Isaac Barreto <isaacssb123@gmail.com>
2022-09-15 20:59:47 -0300

    Principio da Responsabilidade Unica

---
+++ src/Model/Feedback.php
@@ -0,0 +1,28 @@
+<?php
+namespace Alura\Solid\Model;
+
+class Feedback
+{
+    private $nota;
+    private $depoimento;
+
+    public function __construct(int $nota, ?string $depoimento)
+    {
+        if ($nota < 9 && empty($depoimento)) {
+            throw new \DomainException('Depoimento obrigatório');
+        }
+
+        $this->nota = $nota;
+        $this->depoimento = $depoimento;
+    }
+
+    public function recuperarNota(): int
+    {
+        return $this->nota;
+    }
+
+    public function recuperarDepoimento(): ?string
+    {
+        return $this->depoimento;
+    }
+}
